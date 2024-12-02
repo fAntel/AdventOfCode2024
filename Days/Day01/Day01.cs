d609c2d78ad34da701eac6e12353a7f3ed945338
Anton Kovalyov <keldzh@gmail.com>
2024-12-02 23:04:37 +0300

    Day 01: Historian Hysteria

---
+++ Days/Day01/Day01.cs
@@ -0,0 +1,78 @@
+﻿using AdventOfCode2024.Common;
+
+namespace AdventOfCode2024.Days.Day01
+{
+    public class Day01 : IDay
+    {
+        public string PartOne()
+        {
+            IEnumerable<IOrderedEnumerable<int>> lists = ReadLists();
+
+            var result = lists.First()
+                .Zip(lists.Last(), (a, b) => Math.Abs(a - b))
+                .Sum();
+
+            return result.ToString();
+        }
+
+        public string PartTwo()
+        {
+            IEnumerable<IOrderedEnumerable<int>> lists = ReadLists();
+
+            var firstList = lists.First();
+            var secondList = lists.Last().ToArray();
+            int i = 0;
+            int counter = 0;
+            int? prevItem = null;
+            long result = 0;
+            foreach (var item in firstList)
+            {
+                if (prevItem != null && prevItem == item)
+                {
+                    result += item * counter;
+                    continue;
+                }
+
+                counter = 0;
+
+                while (i < secondList.Length && secondList[i] < item)
+                    ++i;
+                if (i >= secondList.Length)
+                    break;
+
+                while (i < secondList.Length && item == secondList[i])
+                {
+                    ++i;
+                    ++counter;
+                }
+
+                result += item * counter;
+
+                if (i >= secondList.Length)
+                    break;
+
+                if (i > 0)
+                    --i;
+
+                prevItem = item;
+            }
+
+            return result.ToString();
+        }
+
+        private static IEnumerable<IOrderedEnumerable<int>> ReadLists()
+        {
+            return File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Days", "Day01", "input"))
+                .Select(line => line.Split(' '))
+                .Select(line => line.Where(v => v.Length > 0))
+                .Select(values => values.Select(s => int.Parse(s)))
+                .Aggregate(new List<List<int>>(2) { new(), new() }, (acc, v) =>
+                {
+                    acc[0].Add(v.First());
+                    acc[1].Add(v.Last());
+                    return acc;
+                })
+                .Select(list => list.Order());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using AdventOfCode2024.Common;
+using AdventOfCode2024.Days.Day01;
+
+IDay day = new Day01();
+Console.WriteLine(day.PartOne());
+Console.WriteLine(day.PartTwo());
