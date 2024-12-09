b55dae6d2f09fc8c9556e49e654b46913b327c14
Anton Kovalyov <keldzh@gmail.com>
2024-12-10 00:41:06 +0300

    Day 05: Print Queue. Part Two

--- Days/Day05/Day05.cs
+++ Days/Day05/Day05.cs
@@ -1,10 +1,4 @@
 ﻿using AdventOfCode2024.Common;
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode2024.Days.Day05
 {
@@ -12,65 +6,140 @@ namespace AdventOfCode2024.Days.Day05
     {
         public string PartOne()
         {
-            var input = File.ReadLines(InputHelper.GetInputPath(5));
-            var orderingRules = input
-                .TakeWhile(line => line.Length > 0) // take only ordering rules
-                .Aggregate(new Dictionary<int, ISet<int>>(), (acc, line) =>
-                {
-                    string[] substrs = line.Split('|');
-                    int firstPageNumber = int.Parse(substrs[0]);
-                    ISet<int>? secondPageNumers;
-                    if (!acc.TryGetValue(firstPageNumber, out secondPageNumers))
-                    {
-                        secondPageNumers = new HashSet<int>();
-                        acc.Add(firstPageNumber, secondPageNumers);
-                    }
-                    secondPageNumers.Add(int.Parse(substrs[1]));
-                    return acc;
-                });
-            var updates = input
-                .SkipWhile(line => line.Length > 0) // skip ordering rules
-                .Skip(1)                            // skip empty line between ordering rules and updates
-                .Select(line => line.Split(",").Select(pageNumber => int.Parse(pageNumber)));
+            Dictionary<int, ISet<int>> orderingRules;
+            List<List<int>> updates;
+            ParseInput(out orderingRules, out updates);
 
-            List<int> middlePages = [];
-            ISet<int>? yPages;
-            ISet<int> passedPages = new HashSet<int>();
-            int count;
+            int result = 0;
             foreach (var update in updates)
             {
-                count = 0;
-                passedPages.Clear();
-                foreach (var page in update)
-                {
-                    if (passedPages.Count > 0)
-                    {
-                        if (orderingRules.TryGetValue(page, out yPages))
-                        {
-                            if (yPages.Intersect(passedPages).Count() > 0)
-                            {
-                                count = 0;
-                                break;
-                            }
-                        }
-                    }
+                Dictionary<WrongPage, ISet<int>> wrongUpdates = CheckUpdates(update, orderingRules, returnOnWrong: true);
 
-                    passedPages.Add(page);
-                    ++count;
-                }
-
-                if (count > 0)
+                if (wrongUpdates.Count == 0)
                 {
-                    middlePages.Add(update.Skip(count / 2).First());
+                    result += update[update.Count / 2];
                 }
             }
 
-            return middlePages.Sum().ToString();
+            return result.ToString();
         }
 
         public string PartTwo()
         {
-            throw new NotImplementedException();
+            Dictionary<int, ISet<int>> orderingRules;
+            List<List<int>> updates;
+            ParseInput(out orderingRules, out updates);
+
+            int result = 0;
+            foreach (var update in updates)
+            {
+                Dictionary<WrongPage, ISet<int>> wrongUpdates;
+                wrongUpdates = CheckUpdates(update, orderingRules, returnOnWrong: false);
+
+                if (wrongUpdates.Count == 0)
+                    continue;
+
+                ISet<int> rightPages;
+                WrongPage wrongPage;
+                while (wrongUpdates.Count > 0)
+                {
+                    wrongPage = wrongUpdates.First().Key;
+                    rightPages = orderingRules[wrongPage.pageNumber].ToHashSet();
+                    rightPages.UnionWith(wrongUpdates[wrongPage]);
+
+                    for (int i = 0; i < update.Count; ++i)
+                    {
+                        if (rightPages.Contains(update[i]))
+                        {
+                            update.RemoveAt(wrongPage.Index);
+                            update.Insert(i, wrongPage.pageNumber);
+                            wrongUpdates.Remove(wrongPage);
+
+                            Dictionary<WrongPage, ISet<int>> t = wrongUpdates;
+                            wrongUpdates = [];
+                            int wrongPageOldIndex = wrongPage.Index;
+                            int wrongPageNewIndex = i;
+                            foreach (var key in t.Keys)
+                            {
+                                wrongPage = wrongPageNewIndex <= key.Index && key.Index <= wrongPageOldIndex
+                                    ? new WrongPage(key.pageNumber, key.Index + 1)
+                                    : key;
+                                wrongUpdates[wrongPage] = t[key];
+                            }
+
+                            break;
+                        }
+                    }
+                }
+
+                result += update[update.Count / 2];
+            }
+
+            return result.ToString();
+        }
+
+        private static void ParseInput(out Dictionary<int, ISet<int>> orderingRules, out List<List<int>> updates)
+        {
+            orderingRules = [];
+            updates = [];
+            bool hasOrderingRulesFinished = false;
+            foreach (var line in File.ReadLines(InputHelper.GetInputPath(5)))
+            {
+                if (line.Length == 0)
+                {
+                    hasOrderingRulesFinished = true;
+                    continue;
+                }
+                if (!hasOrderingRulesFinished)
+                {
+                    string[] substrs = line.Split('|');
+                    int firstPageNumber = int.Parse(substrs[0]);
+                    if (!orderingRules.TryGetValue(firstPageNumber, out ISet<int>? secondPageNumers))
+                    {
+                        secondPageNumers = new HashSet<int>();
+                        orderingRules.Add(firstPageNumber, secondPageNumers);
+                    }
+                    secondPageNumers.Add(int.Parse(substrs[1]));
+                }
+                else
+                {
+                    updates.Add(line.Split(",").Aggregate(new List<int>(), (acc, pageNumber) => { acc.Add(int.Parse(pageNumber)); return acc; }));
+                }
+
+            }
+        }
+
+        private static Dictionary<WrongPage, ISet<int>> CheckUpdates(List<int> update, Dictionary<int, ISet<int>> orderingRules, bool returnOnWrong = true)
+        {
+            ISet<int> passedPages = new HashSet<int>();
+            Dictionary<WrongPage, ISet<int>> wrongPages = [];
+            for (int i = 0; i < update.Count; i++)
+            {
+                int page = update[i];
+                if (passedPages.Count > 0)
+                {
+                    ISet<int>? yPages;
+                    if (orderingRules.TryGetValue(page, out yPages))
+                    {
+                        IEnumerable<int> pagesShouldBeEarlier = yPages.Intersect(passedPages);
+                        if (pagesShouldBeEarlier.Any())
+                        {
+                            wrongPages.Add(new WrongPage(page, i), pagesShouldBeEarlier.ToHashSet());
+                            if (returnOnWrong)
+                                break;
+                        }
+                    }
+                }
+
+                passedPages.Add(page);
+            }
+            return wrongPages;
+        }
+
+        private struct WrongPage(int pageNumber, int index)
+        {
+            public int pageNumber = pageNumber;
+            public int Index = index;
         }
     }
 }
